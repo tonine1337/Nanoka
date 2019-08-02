974efa8cce9828ce4195893565788278d4a7ad93
phosphene47 <phosphene47@chiya.dev>
2019-08-02 21:06:21 +1000

    Hardcode settings defaults

--- nanoka-client/Nanoka.Core/NanokaOptions.cs
+++ nanoka-client/Nanoka.Core/NanokaOptions.cs
@@ -4,9 +4,9 @@ namespace Nanoka.Core
 
     public class IpfsOptions
     {
-        public string ApiEndpoint { get; set; }
-        public string GatewayEndpoint { get; set; }
-        public string DaemonFlags { get; set; }
-        public double DaemonWaitTimeout { get; set; }
+        public string ApiEndpoint { get; set; } = "localhost:5001";
+        public string GatewayEndpoint { get; set; } = "localhost:8080";
+        public string DaemonFlags { get; set; } = "--init --migrate --enable-gc --writable";
+        public double DaemonWaitTimeout { get; set; } = 10;
     }
 }
