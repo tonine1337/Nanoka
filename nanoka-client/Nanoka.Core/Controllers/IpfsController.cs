a758f2d171803d5a6559f3a7aaa46527af56c553
phosphene47 <phosphene47@chiya.dev>
2019-08-02 21:52:35 +1000

    Add IpfsController

---
+++ nanoka-client/Nanoka.Core/Controllers/IpfsController.cs
@@ -0,0 +1,34 @@
+using System.IO;
+using System.Threading.Tasks;
+using Ipfs.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Nanoka.Core.Controllers
+{
+    [ApiController]
+    [Route("fs")]
+    public class IpfsController : ControllerBase
+    {
+        readonly IpfsClient _client;
+
+        public IpfsController(IpfsClient client)
+        {
+            _client = client;
+        }
+
+        [HttpGet("{*path}")]
+        public async Task<FileStreamResult> GetAsync(string path)
+        {
+            // split name and ext
+            var extension   = Path.GetExtension(path) ?? "";
+            var contentType = MimeTypeMap.GetMimeType(extension);
+
+            if (!path.Contains("/"))
+                path = path.Substring(0, path.Length - extension.Length);
+
+            var stream = await _client.FileSystem.ReadFileAsync(path);
+
+            return new FileStreamResult(stream, contentType);
+        }
+    }
+}
