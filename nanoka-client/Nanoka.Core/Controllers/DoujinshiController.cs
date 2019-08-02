cd523b43e7798487b542962faf77457e55c5f6fd
phosphene47 <phosphene47@chiya.dev>
2019-08-02 22:25:58 +1000

    Add an endpoint for retrieving doujinshi

---
+++ nanoka-client/Nanoka.Core/Controllers/DoujinshiController.cs
@@ -0,0 +1,37 @@
+using System.IO;
+using System.Threading.Tasks;
+using Ipfs.Http;
+using Microsoft.AspNetCore.Mvc;
+using Nanoka.Core.Models;
+using Newtonsoft.Json;
+
+namespace Nanoka.Core.Controllers
+{
+    [ApiController]
+    [Route("doujinshi")]
+    public class DoujinshiController : ControllerBase
+    {
+        readonly IpfsClient _ipfs;
+        readonly JsonSerializer _serializer;
+
+        public DoujinshiController(IpfsClient ipfs, JsonSerializer serializer)
+        {
+            _ipfs       = ipfs;
+            _serializer = serializer;
+        }
+
+        [HttpGet("{cid}")]
+        public async Task<Result<Doujinshi>> GetAsync(string cid)
+        {
+            using (var stream = await _ipfs.FileSystem.ReadFileAsync(cid))
+            using (var reader = new StreamReader(stream))
+            {
+                var doujinshi = _serializer.Deserialize<Doujinshi>(reader);
+
+                doujinshi.Cid = cid;
+
+                return doujinshi;
+            }
+        }
+    }
+}
