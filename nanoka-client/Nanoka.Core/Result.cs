0619c3299642b697fb2e96e3b721fd359703026e
phosphene47 <phosphene47@chiya.dev>
2019-08-03 17:10:37 +1000

    Remove StatusDescription

--- nanoka-client/Nanoka.Core/Result.cs
+++ nanoka-client/Nanoka.Core/Result.cs
@@ -21,27 +21,23 @@ namespace Nanoka.Core
 
     public class Result<T> : IActionResult
     {
-        [JsonProperty]
+        [JsonProperty("error")]
         public bool Error => !(200 <= Status && Status < 300);
 
-        [JsonProperty]
+        [JsonProperty("status")]
         public int Status { get; }
 
-        [JsonProperty]
-        public string StatusDescription { get; }
-
-        [JsonProperty]
+        [JsonProperty("message")]
         public string Message { get; }
 
-        [JsonProperty]
+        [JsonProperty("body")]
         public T Body { get; }
 
         public Result(HttpStatusCode status, string message, T body)
         {
-            Status            = (int) status;
-            StatusDescription = status.ToString();
-            Message           = message;
-            Body              = body;
+            Status  = (int) status;
+            Message = message ?? status.ToString();
+            Body    = body;
         }
 
         public static implicit operator Result<T>(T value) => new Result<T>(HttpStatusCode.OK, null, value);
