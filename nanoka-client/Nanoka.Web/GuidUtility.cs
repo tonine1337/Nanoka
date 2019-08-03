814c7fb537dfbf2c44f79959e18ff016a0f63f3c
phosphene47 <phosphene47@chiya.dev>
2019-08-03 20:21:30 +1000

    Add GuidUtility

---
+++ nanoka-client/Nanoka.Web/GuidUtility.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Nanoka.Web
+{
+    public static class GuidUtility
+    {
+        public static string ToShortString(this Guid guid)
+            => Convert.ToBase64String(guid.ToByteArray()).Substring(0, 22);
+
+        public static Guid ToGuid(this string str) => new Guid(Convert.FromBase64String(str + "=="));
+    }
+}
