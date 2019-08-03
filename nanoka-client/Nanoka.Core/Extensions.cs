5bd283fba8320ffa3505bc1cff0055a4e233a878
phosphene47 <phosphene47@chiya.dev>
2019-08-03 21:45:50 +1000

    Add some linq ext

--- nanoka-client/Nanoka.Core/Extensions.cs
+++ nanoka-client/Nanoka.Core/Extensions.cs
@@ -1,10 +1,19 @@
+using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using Newtonsoft.Json;
 
 namespace Nanoka.Core
 {
     public static class Extensions
     {
+        public static T2[] ToArray<T1, T2>(this IEnumerable<T1> enumerable, Func<T1, T2> selector)
+            => enumerable.Select(selector).ToArray();
+
+        public static List<T2> ToList<T1, T2>(this IEnumerable<T1> enumerable, Func<T1, T2> selector)
+            => enumerable.Select(selector).ToList();
+
         public static T Deserialize<T>(this JsonSerializer serializer, TextReader reader)
         {
             using (var jsonReader = new JsonTextReader(reader))
