1151b5a9f5e5270882841e0e6b9540d88df451d0
phosphene47 <phosphene47@chiya.dev>
2019-08-05 20:37:04 +1000

    Combine RangeQuery.cs and RangeQueryItem

--- nanoka-client/Nanoka.Core/Models/Query/RangeQuery.cs
+++ nanoka-client/Nanoka.Core/Models/Query/RangeQuery.cs
@@ -19,4 +19,16 @@ namespace Nanoka.Core.Models.Query
                Values.Length != 0 &&
                Values.Any(x => x.Min != null || x.Max != null);
     }
+
+    public class RangeQueryItem<T> where T : struct
+    {
+        [JsonProperty("min")]
+        public T? Min { get; set; }
+
+        [JsonProperty("max")]
+        public T? Max { get; set; }
+
+        [JsonProperty("exclusive")]
+        public bool Exclusive { get; set; }
+    }
 }
