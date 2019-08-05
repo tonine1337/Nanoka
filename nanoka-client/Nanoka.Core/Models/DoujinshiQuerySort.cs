9fc9f8b8d6eac897f6c677857e33ad8f1319a8ab
phosphene47 <phosphene47@chiya.dev>
2019-08-05 20:19:18 +1000

    Disallow sorting by meta fields

--- nanoka-client/Nanoka.Core/Models/DoujinshiQuerySort.cs
+++ nanoka-client/Nanoka.Core/Models/DoujinshiQuerySort.cs
@@ -14,15 +14,6 @@ namespace Nanoka.Core.Models
         [EnumMember(Value = "name_romanized")] RomanizedName,
         [EnumMember(Value = "name_english")] EnglishName,
         [EnumMember(Value = "score")] Score,
-        [EnumMember(Value = "artist")] Artist,
-        [EnumMember(Value = "group")] Group,
-        [EnumMember(Value = "parody")] Parody,
-        [EnumMember(Value = "character")] Character,
-        [EnumMember(Value = "category")] Category,
-        [EnumMember(Value = "language")] Language,
-        [EnumMember(Value = "tag")] Tag,
-        [EnumMember(Value = "convention")] Convention,
-        [EnumMember(Value = "source")] Source,
         [EnumMember(Value = "pages")] PageCount
     }
 }
