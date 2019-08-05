52fb1327263a801cec5a47b0f80b2e27739c641b
phosphene47 <phosphene47@chiya.dev>
2019-08-05 23:37:38 +1000

    Set development secret

--- nanoka-client/Nanoka.Web/NanokaOptions.cs
+++ nanoka-client/Nanoka.Web/NanokaOptions.cs
@@ -4,7 +4,8 @@ namespace Nanoka.Web
 {
     public class NanokaOptions
     {
-        public string Secret { get; set; }
+        // development secret; should be overridden in production to something safer
+        public string Secret { get; set; } = "OAcB&fhvjYfb#iHPG4E33OEAV*1X!5jFeZsXvB9zKLsZ$wZWe5$WK75x9t!R";
 
         public string ElasticEndpoint { get; set; }
         public string ElasticIndexPrefix { get; set; } = "nanoka-";
