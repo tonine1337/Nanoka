7055e79cbe0b3fa389e54d0176c0f6d6c3cb1d40
phosphene47 <phosphene47@chiya.dev>
2019-08-05 22:49:10 +1000

    Fix Version field name not specified

--- nanoka-client/Nanoka.Core/Models/DatabaseInfo.cs
+++ nanoka-client/Nanoka.Core/Models/DatabaseInfo.cs
@@ -1,7 +1,10 @@
+using Newtonsoft.Json;
+
 namespace Nanoka.Core.Models
 {
     public class DatabaseInfo
     {
+        [JsonProperty("version")]
         public NanokaVersion Version { get; set; }
     }
 }
