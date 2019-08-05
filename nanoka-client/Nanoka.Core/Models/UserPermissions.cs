f4e2fca688bd7fa6cf8d5aee36a3af015eaf0843
phosphene47 <phosphene47@chiya.dev>
2019-08-05 22:53:11 +1000

    Add admin flag

--- nanoka-client/Nanoka.Core/Models/UserPermissions.cs
+++ nanoka-client/Nanoka.Core/Models/UserPermissions.cs
@@ -3,5 +3,17 @@ using System;
 namespace Nanoka.Core.Models
 {
     [Flags]
-    public enum UserPermissions { }
+    public enum UserPermissions
+    {
+        /// <summary>
+        /// User has no special permission.
+        /// </summary>
+        None = 0,
+
+        /// <summary>
+        /// User is an administrator.
+        /// This flag effectively overrides every other flag with 1.
+        /// </summary>
+        Administrator = 1 << 0
+    }
 }
