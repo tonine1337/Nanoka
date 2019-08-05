6c53044266024d70ad048dcc2dc3ddad7ef8f42c
phosphene47 <phosphene47@chiya.dev>
2019-08-05 23:10:08 +1000

    Make ToShortString url-safe

--- nanoka-client/Nanoka.Web/GuidUtility.cs
+++ nanoka-client/Nanoka.Web/GuidUtility.cs
@@ -5,8 +5,12 @@ namespace Nanoka.Web
     public static class GuidUtility
     {
         public static string ToShortString(this Guid guid)
-            => Convert.ToBase64String(guid.ToByteArray()).Substring(0, 22);
+            => Convert.ToBase64String(guid.ToByteArray())
+                      .Substring(0, 22)
+                      .Replace("/", "_")
+                      .Replace("+", "-");
 
-        public static Guid ToGuid(this string str) => new Guid(Convert.FromBase64String(str + "=="));
+        public static Guid ToGuid(this string str)
+            => new Guid(Convert.FromBase64String(str.Replace("_", "/").Replace("-", "+") + "=="));
     }
 }
