da1f2c8003be65826cd0b7b661e8cdacdf360ed7
phosphene47 <phosphene47@chiya.dev>
2019-08-06 22:19:06 +1000

    Add an endpoint for modifying doujinshi

--- nanoka-client/Nanoka.Web/Controllers/DoujinshiController.cs
+++ nanoka-client/Nanoka.Web/Controllers/DoujinshiController.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Threading.Tasks;
+using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Options;
 using Nanoka.Core;
@@ -14,11 +15,13 @@ namespace Nanoka.Web.Controllers
     {
         readonly NanokaOptions _options;
         readonly NanokaDatabase _db;
+        readonly IMapper _mapper;
 
-        public DoujinshiController(IOptions<NanokaOptions> options, NanokaDatabase db)
+        public DoujinshiController(IOptions<NanokaOptions> options, NanokaDatabase db, IMapper mapper)
         {
             _options = options.Value;
             _db      = db;
+            _mapper  = mapper;
         }
 
         [HttpGet("{id}")]
@@ -30,6 +33,50 @@ namespace Nanoka.Web.Controllers
             query.Limit = Math.Min(query.Limit, _options.MaxResultCount);
 
             return await _db.SearchAsync(query);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<Result<Doujinshi>> UpdateAsync(Guid id, DoujinshiBase model)
+        {
+            var now = DateTime.UtcNow;
+
+            var doujinshi = await _db.GetDoujinshiAsync(id);
+
+            if (doujinshi == null)
+            {
+                // new doujinshi is being created
+                doujinshi = new Doujinshi
+                {
+                    Id         = id,
+                    UploadTime = now
+                };
+            }
+
+            else
+            {
+                // we are updating an existing doujinshi
+                // create a snapshot first
+                var snapshot = new Snapshot<Doujinshi>
+                {
+                    Id          = Guid.NewGuid(),
+                    TargetId    = doujinshi.Id,
+                    CommitterId = UserId,
+                    Time        = now,
+                    Value       = doujinshi
+                };
+
+                await _db.IndexSnapshotAsync(snapshot);
+            }
+
+            // set new values
+            _mapper.Map(model, doujinshi);
+
+            doujinshi.UpdateTime = now;
+
+            // update database
+            await _db.IndexAsync(doujinshi);
+
+            return doujinshi;
         }
     }
 }
