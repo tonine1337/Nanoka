87c21a7bce992c4f3296cfb9609751e2f5771c93
phosphene47 <phosphene47@chiya.dev>
2019-08-06 22:42:32 +1000

    Add an endpoint to create a variant

--- nanoka-client/Nanoka.Web/Controllers/DoujinshiController.cs
+++ nanoka-client/Nanoka.Web/Controllers/DoujinshiController.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
@@ -35,48 +36,69 @@ namespace Nanoka.Web.Controllers
             return await _db.SearchAsync(query);
         }
 
+        async Task CreateSnapshotAsync(Doujinshi doujinshi)
+        {
+            var snapshot = new Snapshot<Doujinshi>
+            {
+                Id          = Guid.NewGuid(),
+                TargetId    = doujinshi.Id,
+                CommitterId = UserId,
+                Time        = DateTime.Now,
+                Value       = doujinshi
+            };
+
+            await _db.IndexSnapshotAsync(snapshot);
+        }
+
         [HttpPut("{id}")]
-        public async Task<Result<Doujinshi>> UpdateAsync(Guid id, DoujinshiBase model)
+        public async Task<Result<Doujinshi>> UpdateDoujinshiAsync(Guid id, DoujinshiBase model)
         {
             var now = DateTime.UtcNow;
 
             var doujinshi = await _db.GetDoujinshiAsync(id);
 
             if (doujinshi == null)
-            {
-                // new doujinshi is being created
                 doujinshi = new Doujinshi
                 {
                     Id         = id,
                     UploadTime = now
                 };
-            }
 
             else
-            {
-                // we are updating an existing doujinshi
-                // create a snapshot first
-                var snapshot = new Snapshot<Doujinshi>
-                {
-                    Id          = Guid.NewGuid(),
-                    TargetId    = doujinshi.Id,
-                    CommitterId = UserId,
-                    Time        = now,
-                    Value       = doujinshi
-                };
+                await CreateSnapshotAsync(doujinshi);
 
-                await _db.IndexSnapshotAsync(snapshot);
-            }
-
-            // set new values
             _mapper.Map(model, doujinshi);
 
             doujinshi.UpdateTime = now;
 
-            // update database
             await _db.IndexAsync(doujinshi);
 
             return doujinshi;
+        }
+
+        [HttpPost("{id}/variants")]
+        public async Task<Result<DoujinshiVariant>> CreateVariantAsync(Guid id, DoujinshiVariantBase model)
+        {
+            var doujinshi = await _db.GetDoujinshiAsync(id);
+
+            if (doujinshi == null)
+                return Result.NotFound<Doujinshi>(id);
+
+            await CreateSnapshotAsync(doujinshi);
+
+            var variant = new DoujinshiVariant
+            {
+                UploaderId = UserId
+            };
+
+            _mapper.Map(model, variant);
+
+            doujinshi.Variants = doujinshi.Variants ?? new List<DoujinshiVariant>();
+            doujinshi.Variants.Add(variant);
+
+            await _db.IndexAsync(doujinshi);
+
+            return variant;
         }
     }
 }
