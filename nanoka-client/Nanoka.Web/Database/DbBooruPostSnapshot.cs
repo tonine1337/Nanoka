2862b4285d627475bdf862818b3b9532f5951d43
phosphene47 <phosphene47@chiya.dev>
2019-08-06 21:46:43 +1000

    Fix DbBooruPostSnapshot not defining ElasticsearchType

--- nanoka-client/Nanoka.Web/Database/DbBooruPostSnapshot.cs
+++ nanoka-client/Nanoka.Web/Database/DbBooruPostSnapshot.cs
@@ -1,9 +1,11 @@
 using Nanoka.Core;
 using Nanoka.Core.Models;
+using Nest;
 using Newtonsoft.Json;
 
 namespace Nanoka.Web.Database
 {
+    [ElasticsearchType(RelationName = nameof(BooruPostSnapshot), IdProperty = nameof(Id))]
     public class DbBooruPostSnapshot : DbSnapshotBase<DbBooruPostSnapshot, BooruPostSnapshot, BooruPost>
     {
         protected override string Serialize(JsonSerializer serializer, BooruPost value)
