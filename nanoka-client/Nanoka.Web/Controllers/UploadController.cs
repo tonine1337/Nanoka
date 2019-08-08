d4a98f3facf1a66600809d7de94e9c9d7bf41de3
phosphene47 <phosphene47@chiya.dev>
2019-08-08 21:15:07 +1000

    Add an endpoint for retrieving uploader status

---
+++ nanoka-client/Nanoka.Web/Controllers/UploadController.cs
@@ -0,0 +1,30 @@
+using System;
+using Microsoft.AspNetCore.Mvc;
+using Nanoka.Core;
+using Nanoka.Core.Models;
+
+namespace Nanoka.Web.Controllers
+{
+    [ApiController]
+    [Route("uploads")]
+    public class UploadController : AuthorizedControllerBase
+    {
+        readonly UploadManager _uploadManager;
+
+        public UploadController(UploadManager uploadManager)
+        {
+            _uploadManager = uploadManager;
+        }
+
+        [HttpGet("{workerId}")]
+        public Result<UploadState<object>> GetState(Guid workerId)
+        {
+            var worker = _uploadManager.FindWorker(workerId);
+
+            if (worker == null)
+                return Result.NotFound<UploadWorker>(workerId);
+
+            return worker.CreateState<object>();
+        }
+    }
+}
