5677573bc4892255ad30cdf24f3e6b91e1782f28
phosphene47 <phosphene47@chiya.dev>
2019-08-08 17:41:56 +1000

    Add UploadState model

---
+++ nanoka-client/Nanoka.Core/Models/UploadState.cs
@@ -0,0 +1,26 @@
+using System;
+using Newtonsoft.Json;
+
+namespace Nanoka.Core.Models
+{
+    public class UploadState
+    {
+        [JsonProperty("id")]
+        public Guid Id { get; set; }
+
+        [JsonProperty("progress")]
+        public double Progress { get; set; }
+
+        [JsonProperty("start")]
+        public DateTime Start { get; set; }
+
+        [JsonProperty("end")]
+        public DateTime End { get; set; }
+
+        [JsonProperty("running")]
+        public bool IsRunning { get; set; }
+
+        [JsonProperty("message")]
+        public string Message { get; set; }
+    }
+}
