39b8a97fbe94d59a06026dbc845b6fa7916e1a45
phosphene47 <phosphene47@chiya.dev>
2019-08-11 22:43:18 +1000

    Implement all handler methods as endpoints

--- nanoka-client/Nanoka.Client/Controllers/DoujinshiController.cs
+++ nanoka-client/Nanoka.Client/Controllers/DoujinshiController.cs
@@ -1,5 +1,9 @@
 using System;
+using System.IO.Compression;
+using System.Linq;
 using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Nanoka.Core;
 using Nanoka.Core.Client;
@@ -12,13 +16,108 @@ namespace Nanoka.Client.Controllers
     public class DoujinshiController : ControllerBase
     {
         readonly IDatabaseClient _client;
+        readonly IMapper _mapper;
 
-        public DoujinshiController(IDatabaseClient client)
+        public DoujinshiController(IDatabaseClient client, IMapper mapper)
         {
             _client = client;
+            _mapper = mapper;
         }
 
-        [HttpGet("{id}")]
-        public async Task<Result<Doujinshi>> GetAsync(Guid id) => await _client.Doujinshi.GetAsync(id);
+        [HttpPost("search")]
+        public async Task<Result<SearchResult<Doujinshi>>> SearchAsync(DoujinshiQuery query)
+            => await _client.Doujinshi.SearchAsync(q => query);
+
+        public class UploadDoujinshiRequest
+        {
+            [FromForm(Name = "doujinshi")]
+            public DoujinshiBase Doujinshi { get; set; }
+
+            [FromForm(Name = "variant")]
+            public DoujinshiVariantBase Variant { get; set; }
+
+            [FromForm(Name = "file")]
+            public IFormFile Archive { get; set; }
+        }
+
+        [HttpPost]
+        public async Task<Result<Guid>> UploadAsync([FromForm] UploadDoujinshiRequest request)
+        {
+            using (var stream = request.Archive.OpenReadStream())
+            using (var archive = new ZipArchive(stream, ZipArchiveMode.Read, false))
+            {
+                var task = await _client.Doujinshi.UploadAsync(request.Doujinshi, request.Variant, archive);
+
+                return task.Id;
+            }
+        }
+
+        [HttpPut("{id}")]
+        public async Task<Result<Doujinshi>> UpdateAsync(Guid id, DoujinshiBase model)
+        {
+            var doujinshi = await _client.Doujinshi.GetAsync(id);
+
+            if (doujinshi == null)
+                return Result.NotFound<Doujinshi>(id);
+
+            _mapper.Map(model, doujinshi);
+
+            await _client.Doujinshi.UpdateAsync(doujinshi);
+
+            return doujinshi;
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<Result> DeleteAsync(Guid id, [FromQuery] string reason)
+        {
+            var doujinshi = await _client.Doujinshi.GetAsync(id);
+
+            if (doujinshi == null)
+                return Result.NotFound<Doujinshi>(id);
+
+            await _client.Doujinshi.DeleteAsync(doujinshi, reason);
+
+            return Result.Ok();
+        }
+
+        public class UploadVariantRequest
+        {
+            [FromForm(Name = "variant")]
+            public DoujinshiVariantBase Variant { get; set; }
+
+            [FromForm(Name = "file")]
+            public IFormFile Archive { get; set; }
+        }
+
+        [HttpPost("{id}/variants")]
+        public async Task<Result<Guid>> UploadVariantAsync(Guid id, [FromForm] UploadVariantRequest request)
+        {
+            var doujinshi = await _client.Doujinshi.GetAsync(id);
+
+            if (doujinshi == null)
+                return Result.NotFound<Doujinshi>(id);
+
+            using (var stream = request.Archive.OpenReadStream())
+            using (var archive = new ZipArchive(stream, ZipArchiveMode.Read, false))
+            {
+                var task = await _client.Doujinshi.UploadVariantAsync(doujinshi, request.Variant, archive);
+
+                return task.Id;
+            }
+        }
+
+        [HttpDelete("{id}/variants/{variantId}")]
+        public async Task<Result> DeleteVariantAsync(Guid id, Guid variantId)
+        {
+            var doujinshi = await _client.Doujinshi.GetAsync(id);
+            var variant   = doujinshi?.Variants.FirstOrDefault(v => v.Id == variantId);
+
+            if (variant == null)
+                return Result.NotFound<DoujinshiVariant>(id, variantId);
+
+            await _client.Doujinshi.DeleteVariantAsync(doujinshi, variant);
+
+            return Result.Ok();
+        }
     }
 }
