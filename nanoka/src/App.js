8b8480854f260ec8cc1f9289db67978f588bcb13
phosphene47 <phosphene47@chiya.dev>
2019-08-12 21:50:52 +1000

    Hugely improve landing page

--- nanoka/src/App.js
+++ nanoka/src/App.js
@@ -1,116 +1,150 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, NavLink } from "react-router-dom";
 import Index from './Index/Index';
+import { Button, Icon, Menu, Header, Divider, Input, Container } from 'semantic-ui-react';
 
-function App() {
-  return (
-    <Router>
-      <div>
-        <div>
-          <div className="ui menu">
-            <a href="/" className="header item">Nanoka</a>
-            <div className="ui simple dropdown item">
-              <i className="icon book"></i>
-              Doujinshi
-              <div className="menu" style={{ minWidth: "15em" }}>
-                <div className="header">Doujinshi</div>
-                <NavLink to="/doujinshi/listby/name" className="item"><i className="icon sort alphabet down"></i> Name</NavLink>
-                <NavLink to="/doujinshi/listby/upload" className="item"><i className="icon clock"></i> Newest</NavLink>
-                <NavLink to="/doujinshi/listby/trending" className="item"><i className="icon chart line"></i> Trending</NavLink>
-                <NavLink to="/doujinshi/listby/viewed" className="item"><i className="icon eye"></i> Most Viewed</NavLink>
-                <NavLink to="/doujinshi/random" className="item"><i className="icon random"></i> Random</NavLink>
-                <div className="divider"></div>
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
 
-                <div className="header">Meta</div>
-                <NavLink to="/doujinshi/listmeta/artist" className="item"><i className="icon paint brush"></i> Artist</NavLink>
-                <NavLink to="/doujinshi/listmeta/group" className="item"><i className="icon users"></i> Group</NavLink>
-                <NavLink to="/doujinshi/listmeta/parody" className="item"><i className="icon linkify"></i> Parody</NavLink>
-                <NavLink to="/doujinshi/listmeta/character" className="item"><i className="icon odnoklassniki"></i> Character</NavLink>
-                <NavLink to="/doujinshi/listmeta/category" className="item"><i className="icon th large"></i> Category</NavLink>
-                <NavLink to="/doujinshi/listmeta/language" className="item"><i className="icon globe"></i> Language</NavLink>
-                <NavLink to="/doujinshi/listmeta/tag" className="item"><i className="icon tag"></i> Tag</NavLink>
-                <NavLink to="/doujinshi/listmeta/convention" className="item"><i className="icon calendar"></i> Convention</NavLink>
-              </div>
-            </div>
-            <div className="ui simple dropdown item">
-              <i className="icon image"></i>
-              Booru
-              <div className="menu" style={{ minWidth: "15em" }}>
-                <div className="header">Booru</div>
-                <NavLink to="/booru/listby/upload" className="item"><i className="icon clock"></i> Newest</NavLink>
-                <NavLink to="/booru/listby/trending" className="item"><i className="icon chart line"></i> Trending</NavLink>
-                <NavLink to="/booru/listby/viewed" className="item"><i className="icon eye"></i> Most Viewed</NavLink>
-                <NavLink to="/booru/random" className="item"><i className="icon random"></i> Random</NavLink>
-                <div className="divider"></div>
+    this.state = {
+      fetched: false,
+      client: null,
+      error: null
+    };
+  }
 
-                <div className="header">Tag</div>
-                <NavLink to="/booru/listtag/general" className="item"><i className="icon tag"></i> General</NavLink>
-                <NavLink to="/booru/listtag/artist" className="item"><i className="icon paint brush"></i> Artist</NavLink>
-                <NavLink to="/booru/listtag/character" className="item"><i className="icon odnoklassniki"></i> Character</NavLink>
-                <NavLink to="/booru/listtag/copyright" className="item"><i className="icon copyright"></i> Copyright</NavLink>
-                <NavLink to="/booru/listtag/metadata" className="item"><i className="icon info"></i> Metadata</NavLink>
-              </div>
-            </div>
-            <div className="ui simple dropdown item">
-              <i className="icon users"></i>
-              Community
-              <div className="menu" style={{ minWidth: "15em" }}>
-                <div className="header">Activity</div>
-                <NavLink to="/activity/edit" className="item"><i className="icon edit"></i> Edits</NavLink>
-                <NavLink to="/activity/comment" className="item"><i className="icon comments"></i> Comments</NavLink>
-                <NavLink to="/activity/note" className="item"><i className="icon sticky note"></i> Notes</NavLink>
-                <NavLink to="/activity/moderation" className="item"><i className="icon user secret"></i> Moderation</NavLink>
-                <div className="divider"></div>
+  componentDidMount() {
+    fetch('https://nanoka.localhost.chiya.dev:7230')
+      .then(r => {
+        this.setState({
+          client: r.json(),
+          fetched: true
+        });
+      })
+      .catch(err => {
+        this.setState({
+          error: err,
+          fetched: true
+        })
+      });
+  }
 
-                <div className="header">Wiki</div>
-                <NavLink to="/wiki" className="item"><i className="icon book"></i> Wiki</NavLink>
-                <div className="divider"></div>
+  render() {
+    if (!this.state.fetched)
+      return null;
 
-                <div className="header">Forum</div>
-                <a href="https://reddit.com" target="_blank" className="item"><i className="icon reddit"></i> Reddit</a>
-                <a href="https://discord.gg/Edx7Rbc" target="_blank" className="item"><i className="icon discord"></i> Discord</a>
-              </div>
-            </div>
-            <div className="right menu">
-              <div className="item">
-                <div className="ui action left icon input">
-                  <i className="search icon"></i>
-                  <input type="text" style={{ minWidth: "30rem" }} />
-                  <button className="ui button primary">Search</button>
-                </div>
-              </div>
-              <div className="ui simple dropdown item">
-                <i className="icon cog"></i>
-                <div className="menu" style={{ minWidth: "15em", marginRight: "2px" }}>
-                  <div className="header">Nanoka</div>
-                  <NavLink exact to="/account" className="item"><i className="icon user"></i> Account</NavLink>
-                  <NavLink to="/account/dashboard" className="item"><i className="icon dashboard"></i> Dashboard</NavLink>
-                  <NavLink to="/account/uploads" className="item"><i className="icon upload"></i> My uploads</NavLink>
-                  <NavLink to="/settings/language" className="item"><i className="icon globe"></i> Language</NavLink>
-                  <NavLink to="/settings/theme" className="item"><i className="icon theme"></i> Theme</NavLink>
-                  <div className="divider"></div>
+    if (this.state.error) {
+      return (
+        <Container as="main">
+          <h1 style={{ paddingTop: "3rem" }}>Nanoka</h1>
+          <span>Could not connect to Nanoka client :(</span>
+          <br />
+          <br />
 
-                  <div className="header">Misc.</div>
-                  <a href="https://github.com/chiyadev/Nanoka" target="_blank" className="item"><i className="icon github"></i> Open Source</a>
-                  <a href="https://github.com/chiyadev/Nanoka" target="_blank" className="item"><i className="icon heart"></i> Support Nanoka</a>
-                  <div className="divider"></div>
+          <Button as="a" href="https://github.com/chiyadev/Nanoka/releases" className="labeled icon primary"><Icon name="download" /> Install</Button>
+          <Button icon labelPosition='left' onClick={() => window.location.reload()}><Icon name='redo' /> Retry</Button>
+          <br />
 
-                  <div className="header">Management</div>
-                  <NavLink to="/management" className="item"><i className="icon database"></i> Database</NavLink>
-                  <a href="/client" className="item"><i className="icon terminal"></i> Open client</a>
-                  <a href="/signout" className="item"><i className="icon sign-out"></i> Sign out</a>
-                </div>
-              </div>
-            </div>
+          <a href="https://github.com/chiyadev/Nanoka"><small>What is this?</small></a>
+        </Container>
+      );
+    }
+
+    return (
+      <Router>
+        <Menu>
+          <Menu.Item as="a" href="/" className="header">Nanoka</Menu.Item>
+          <Menu.Item className="ui simple dropdown">
+            <Icon name="book" /> Doujinshi
+            <Menu style={{ minWidth: "15em" }}>
+              <Header>Doujinshi</Header>
+              <NavLink to="/doujinshi/listby/name" className="item"><Icon name="sort alphabet down" /> Name</NavLink>
+              <NavLink to="/doujinshi/listby/upload" className="item"><Icon name="clock" /> Newest</NavLink>
+              <NavLink to="/doujinshi/listby/trending" className="item"><Icon name="chart line" /> Trending</NavLink>
+              <NavLink to="/doujinshi/listby/viewed" className="item"><Icon name="eye" /> Most Viewed</NavLink>
+              <NavLink to="/doujinshi/random" className="item"><Icon name="random" /> Random</NavLink>
+              <Divider />
+              <Header>Meta</Header>
+              <NavLink to="/doujinshi/listmeta/artist" className="item"><Icon name="paint brush" /> Artist</NavLink>
+              <NavLink to="/doujinshi/listmeta/group" className="item"><Icon name="users" /> Group</NavLink>
+              <NavLink to="/doujinshi/listmeta/parody" className="item"><Icon name="linkify" /> Parody</NavLink>
+              <NavLink to="/doujinshi/listmeta/character" className="item"><Icon name="odnoklassniki" /> Character</NavLink>
+              <NavLink to="/doujinshi/listmeta/category" className="item"><Icon name="th large" /> Category</NavLink>
+              <NavLink to="/doujinshi/listmeta/language" className="item"><Icon name="globe" /> Language</NavLink>
+              <NavLink to="/doujinshi/listmeta/tag" className="item"><Icon name="tag" /> Tag</NavLink>
+              <NavLink to="/doujinshi/listmeta/convention" className="item"><Icon name="calendar" /> Convention</NavLink>
+            </Menu>
+          </Menu.Item>
+          <Menu.Item className="ui simple dropdown">
+            <Icon name="image" /> Booru
+            <Menu style={{ minWidth: "15em" }}>
+              <Header>Booru</Header>
+              <NavLink to="/booru/listby/upload" className="item"><Icon name="clock" /> Newest</NavLink>
+              <NavLink to="/booru/listby/trending" className="item"><Icon name="chart line" /> Trending</NavLink>
+              <NavLink to="/booru/listby/viewed" className="item"><Icon name="eye" /> Most Viewed</NavLink>
+              <NavLink to="/booru/random" className="item"><Icon name="random" /> Random</NavLink>
+              <Divider />
+              <Header>Tag</Header>
+              <NavLink to="/booru/listtag/general" className="item"><Icon name="tag" /> General</NavLink>
+              <NavLink to="/booru/listtag/artist" className="item"><Icon name="paint brush" /> Artist</NavLink>
+              <NavLink to="/booru/listtag/character" className="item"><Icon name="odnoklassniki" /> Character</NavLink>
+              <NavLink to="/booru/listtag/copyright" className="item"><Icon name="copyright" /> Copyright</NavLink>
+              <NavLink to="/booru/listtag/metadata" className="item"><Icon name="info" /> Metadata</NavLink>
+            </Menu>
+          </Menu.Item>
+          <Menu.Item className="ui simple dropdown">
+            <Icon name="users" /> Community
+            <Menu style={{ minWidth: "15em" }}>
+              <Header>Activity</Header>
+              <NavLink to="/activity/edit" className="item"><Icon name="edit" /> Edits</NavLink>
+              <NavLink to="/activity/comment" className="item"><Icon name="comments" /> Comments</NavLink>
+              <NavLink to="/activity/note" className="item"><Icon name="sticky note" /> Notes</NavLink>
+              <NavLink to="/activity/moderation" className="item"><Icon name="user secret" /> Moderation</NavLink>
+              <Divider />
+              <Header>Wiki</Header>
+              <NavLink to="/wiki" className="item"><Icon name="book" /> Wiki</NavLink>
+              <Divider />
+              <Header>Forum</Header>
+              <Menu.Item as="a" href="https://reddit.com" target="_blank" rel="noopener noreferrer" className="item"><Icon name="reddit" /> Reddit</Menu.Item>
+              <Menu.Item as="a" href="https://discord.gg/Edx7Rbc" target="_blank" rel="noopener noreferrer" className="item"><Icon name="discord" /> Discord</Menu.Item>
+            </Menu>
+          </Menu.Item>
+          <div className="right menu">
+            <Menu.Item>
+              <Input className="action left icon">
+                <Icon name="search" />
+                <input type="text" style={{ minWidth: "30rem" }} />
+                <Button className="primary">Search</Button>
+              </Input>
+            </Menu.Item>
+            <Menu.Item className="ui simple dropdown">
+              <Icon name="cog" />
+              <Menu style={{ minWidth: "15em", marginRight: "2px" }}>
+                <Header>Nanoka</Header>
+                <NavLink exact to="/account" className="item"><Icon name="user" /> Account</NavLink>
+                <NavLink to="/account/dashboard" className="item"><Icon name="dashboard" /> Dashboard</NavLink>
+                <NavLink to="/account/uploads" className="item"><Icon name="upload" /> My uploads</NavLink>
+                <NavLink to="/settings/language" className="item"><Icon name="globe" /> Language</NavLink>
+                <NavLink to="/settings/theme" className="item"><Icon name="theme" /> Theme</NavLink>
+                <Divider />
+                <Header>Misc.</Header>
+                <Menu.Item as="a" href="https://github.com/chiyadev/Nanoka" target="_blank" rel="noopener noreferrer" className="item"><Icon name="github" /> Open Source</Menu.Item>
+                <Menu.Item href="https://github.com/chiyadev/Nanoka" target="_blank" rel="noopener noreferrer" className="item"><Icon name="heart" /> Support Nanoka</Menu.Item>
+                <Divider />
+                <Header>Management</Header>
+                <NavLink to="/management" className="item"><Icon name="database" /> Database</NavLink>
+                <Menu.Item href="/client" className="item"><Icon name="terminal" /> Open client</Menu.Item>
+                <Menu.Item href="/signout" className="item"><Icon name="sign-out" /> Sign out</Menu.Item>
+              </Menu>
+            </Menu.Item>
           </div>
-        </div>
+        </Menu>
 
-        <main className="ui container" style={{ marginTop: "3rem", marginBottom: "3rem" }}>
+        <Container as="main" style={{ marginTop: "3rem", marginBottom: "3rem" }}>
           <Route path="/" exact component={Index} />
-        </main>
-      </div>
-    </Router>
-  );
+        </Container>
+      </Router>
+    );
+  }
 }
-
-export default App;
