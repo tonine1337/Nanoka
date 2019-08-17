eb4dd71df6fdcd21010200a60626e3a2593ccaa0
phosphene47 <phosphene47@chiya.dev>
2019-08-17 20:09:49 +1000

    Add extension to format byte count

--- nanoka-server/Nanoka/Extensions.cs
+++ nanoka-server/Nanoka/Extensions.cs
@@ -14,6 +14,57 @@ namespace Nanoka
 {
     public static class Extensions
     {
+        // https://stackoverflow.com/a/11124118
+        // Returns the human-readable file size for an arbitrary, 64-bit file size 
+        // The default format is "0.### XB", e.g. "4.2 KB" or "1.434 GB"
+        public static string GetBytesReadable(long i)
+        {
+            // Get absolute value
+            var absolute = i < 0 ? -i : i;
+            // Determine the suffix and readable value
+            string suffix;
+            double readable;
+            if (absolute >= 0x1000000000000000) // Exabyte
+            {
+                suffix   = "EB";
+                readable = i >> 50;
+            }
+            else if (absolute >= 0x4000000000000) // Petabyte
+            {
+                suffix   = "PB";
+                readable = i >> 40;
+            }
+            else if (absolute >= 0x10000000000) // Terabyte
+            {
+                suffix   = "TB";
+                readable = i >> 30;
+            }
+            else if (absolute >= 0x40000000) // Gigabyte
+            {
+                suffix   = "GB";
+                readable = i >> 20;
+            }
+            else if (absolute >= 0x100000) // Megabyte
+            {
+                suffix   = "MB";
+                readable = i >> 10;
+            }
+            else if (absolute >= 0x400) // Kilobyte
+            {
+                suffix   = "KB";
+                readable = i;
+            }
+            else
+            {
+                return i.ToString("0 B"); // Byte
+            }
+
+            // Divide by 1024 to get fractional value
+            readable = readable / 1024;
+            // Return formatted number with suffix
+            return readable.ToString("0.### ") + suffix;
+        }
+
         public static void Validate(this object obj) => Validator.ValidateObject(obj, new ValidationContext(obj));
 
         public static async Task<IDisposable> EnterAsync(this SemaphoreSlim semaphore,
