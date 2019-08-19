9094a5cea2d248da793541b250602e6fafcc7e82
phosphene47 <phosphene47@chiya.dev>
2019-08-19 20:01:27 +1000

    Update uploader in line with doujinshi format changes

--- nanoka/src/Doujinshi/Uploader.js
+++ nanoka/src/Doujinshi/Uploader.js
@@ -13,13 +13,20 @@ export class Uploader extends React.Component {
   };
 
   render() {
-    const getNameProps = (name, type) => ({
+    const getTextProps = (name, type) => ({
       type: type || 'text',
       onChange: e => this.setState({ [name]: e.target.value }),
       value: this.state[name] || ''
     });
 
-    const getMetaDropProps = name => ({
+    const getSelectProps = (name, options) => ({
+      selection: true,
+      onChange: (_, { value }) => this.setState({ [name]: value }),
+      options: options.map(([text, value]) => ({ text, value })),
+      value: this.state[name] || 0
+    });
+
+    const getTagInputProps = name => ({
       search: true,
       selection: true,
       multiple: true,
@@ -34,127 +41,120 @@ export class Uploader extends React.Component {
       options: this.state[name + 'options'] || [],
       value: this.state[name] || []
     });
-
+    console.log(this.state)
     return (
       <div>
         <h1>Upload a Doujinshi</h1>
         <Form>
           <Form.Group grouped className="two">
             <Form.Field required>
-              <label>Original Name</label>
-              <input placeholder="Original Name" {...getNameProps('originalName')} />
+              <label>Name</label>
+              <input placeholder="Doujinshi name" {...getTextProps('name')} />
               <small>
-                Name of this doujinshi in its source language.
-                This would usually be Japanese.
+                Name of this doujinshi in the written language.
               </small>
             </Form.Field>
             <Form.Field>
               <label>Romanized Name</label>
-              <input placeholder="Romanized Name" {...getNameProps('romanizedName')} />
+              <input placeholder="Romanized name" {...getTextProps('name_romanized')} />
               <small>
-                Romanized version of the original name if applicable.
+                Romanized version of the original name, if applicable.
                 <a href="https://en.wikipedia.org/wiki/Hepburn_romanization" rel="noopener noreferrer" target="_blank"> Hepburn romanization</a> is preferred for Japanese.
               </small>
             </Form.Field>
             <Form.Field>
-              <label>English Name</label>
-              <input placeholder="English Name" {...getNameProps('englishName')} />
+              <label>Source</label>
+              <input placeholder="URL" {...getTextProps('source', 'url')} />
               <small>
-                English translation of the original name if applicable.
+                URL of the web page where this doujinshi is being transferred from.
               </small>
             </Form.Field>
           </Form.Group>
-          <Form.Field required className="four wide">
-            <label>Category</label>
-            <Dropdown selection defaultValue={0} options={[{
-              text: 'Doujinshi',
-              value: 0
-            }, {
-              text: 'Manga',
-              value: 1
-            }, {
-              text: 'Artist CG',
-              value: 2
-            }, {
-              text: 'Game CG',
-              value: 3
-            }, {
-              text: 'Image Set',
-              value: 4
-            }
-            ]} onChange={(_, { value }) => this.setState({ category: value })} />
-            <small>
-              Special category that this doujinshi belongs to.
-            </small>
-          </Form.Field>
+
+          <Form.Group grouped className="four wide">
+            <Form.Field>
+              <label>Language</label>
+              <Dropdown {...getSelectProps('language', [
+                ['Japanese', 0],
+                ['English', 1]
+              ])} />
+              <small>
+                Language in which this doujinshi is written.
+              </small>
+            </Form.Field>
+            <Form.Field>
+              <label>Category</label>
+              <Dropdown {...getSelectProps('category', [
+                ['Doujinshi', 0],
+                ['Manga', 1],
+                ['Artist CG', 2],
+                ['Game CG', 3],
+                ['Image Set', 4]
+              ])} />
+              <small>
+                Special category that this doujinshi belongs to.
+              </small>
+            </Form.Field>
+          </Form.Group>
+
           <Form.Group className="three">
             <Form.Field required>
               <label>Artist</label>
-              <Dropdown placeholder="Artist" {...getMetaDropProps('artist')} />
+              <Dropdown placeholder="Artist" {...getTagInputProps('artist')} />
               <small>
                 Artist of this doujinshi.
               </small>
             </Form.Field>
             <Form.Field>
               <label>Group</label>
-              <Dropdown placeholder="Group" {...getMetaDropProps('group')} />
+              <Dropdown placeholder="Group" {...getTagInputProps('group')} />
               <small>
                 Doujin circle that published this doujinshi.
               </small>
             </Form.Field>
-            <Form.Field required>
-              <label>Language</label>
-              <Dropdown placeholder="Language" {...getMetaDropProps('language')} />
-              <small>
-                Language in which this doujinshi is written, or the target language if translated.
-              </small>
-            </Form.Field>
-          </Form.Group>
-          <Form.Group className="three">
-            <Form.Field>
-              <label>Parody</label>
-              <Dropdown placeholder="Parody" {...getMetaDropProps('parody')} />
-              <small>
-                Name of the anime series, manga or other copyrighted work that this doujinshi is the parody of.
-              </small>
-            </Form.Field>
             <Form.Field>
               <label>Convention</label>
-              <Dropdown placeholder="Convention" {...getMetaDropProps('convention')} />
+              <Dropdown placeholder="Convention" {...getTagInputProps('convention')} />
               <small>
                 Convention where this doujinshi was available.
               </small>
             </Form.Field>
+          </Form.Group>
+
+          <Form.Group className="three">
+            <Form.Field>
+              <label>Parody</label>
+              <Dropdown placeholder="Parody" {...getTagInputProps('parody')} />
+              <small>
+                Name of the anime series, manga or other copyrighted work that this doujinshi is the parody of.
+              </small>
+            </Form.Field>
             <Form.Field required>
               <label>Characters</label>
-              <Dropdown placeholder="Character" {...getMetaDropProps('character')} />
+              <Dropdown placeholder="Character" {...getTagInputProps('character')} />
               <small>
                 Characters that appear in this doujinshi.
               </small>
             </Form.Field>
+            <Form.Field required>
+              <label>Tags</label>
+              <Dropdown placeholder="Tag" {...getTagInputProps('tag')} />
+              <small>
+                Tags that describe this doujinshi appropriately.
+              </small>
+            </Form.Field>
           </Form.Group>
-          <Form.Field required>
-            <label>Tags</label>
-            <Dropdown placeholder="Tag" {...getMetaDropProps('tag')} />
-            <small>
-              Tags that describe this doujinshi appropriately.
-              </small>
-          </Form.Field>
-          <Form.Field>
-            <label>Source</label>
-            <input placeholder="URL" {...getNameProps('source', 'url')} />
-            <small>
-              URL of the web page where this doujinshi was transferred from.
-              </small>
-          </Form.Field>
+
           <Form.Field required>
             <label>File</label>
             {createDropzone.call(this)}
           </Form.Field>
+
           <Button type="submit" className="labeled icon primary" loading={this.state.isSubmitting} onClick={() => this.handleSubmit()}>
             <Icon name="upload" disabled={this.state.isSubmitting} /> Upload
           </Button>
         </Form>
+
         <Message negative hidden={this.state.errors.length === 0}>
           <Message.Header content="There were some errors with your submission." />
           <Message.List items={this.state.errors} />
@@ -178,17 +178,14 @@ export class Uploader extends React.Component {
     if (state.isSubmitting)
       return;
 
+    // validate input first
     const errors = [];
 
-    // validate input first
-    if (!state.originalName)
-      errors.push('Original name is not specified.');
+    if (!state.name)
+      errors.push('Name is not specified.');
 
     if (!state.artist || state.artist.length === 0)
       errors.push('Artist is not specified.');
-
-    if (!state.language || state.language.length === 0)
-      errors.push('Language is not specified.');
 
     if (!state.character || state.character.length === 0)
       errors.push('Doujinshi must have at least one character.');
@@ -200,39 +197,32 @@ export class Uploader extends React.Component {
       errors.push('Please select a file to upload.');
 
     if (errors.length !== 0) {
-      this.setState({
-        isSubmitting: false,
-        errors
-      });
+      this.setState({ isSubmitting: false, errors });
       return;
     }
 
-    this.setState({
-      isSubmitting: true,
-      errors,
-      uploadProgress: 0
-    });
+    this.setState({ isSubmitting: true, errors, uploadProgress: 0 });
 
     const zip = await JSZip.loadAsync(this.state.file);
     const fileCount = Object.keys(zip.files).length;
 
     let upload = await api.createDoujinshiAsync(
       {
-        name_original: state.originalName,
-        name_romanized: state.romanizedName,
-        name_english: state.englishname,
-        category: state.category
-      },
-      {
+        category: state.category,
         metas: {
           artist: state.artist,
           group: state.group,
-          language: state.language,
           parody: state.parody,
           convention: state.convention,
           character: state.character,
           tag: state.tag
         }
+      },
+      {
+        name: state.name,
+        name_romanized: state.name_romanized,
+        language: state.language,
+        source: state.source
       }
     );
 
