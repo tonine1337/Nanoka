724787060db94b969754e5049581cf6286b4bc1b
phosphene47 <phosphene47@chiya.dev>
2019-08-20 22:21:32 +1000

    Imply first image if not specified

--- nanoka/src/Doujinshi/DoujinshiImage.js
+++ nanoka/src/Doujinshi/DoujinshiImage.js
@@ -3,6 +3,6 @@ import * as api from '../Api';
 
 export class DoujinshiImage extends ApiImage {
   async getImageAsync() {
-    return await api.downloadImageAsync(this.props.doujinshi.id, this.props.variant.id, this.props.index);
+    return await api.downloadImageAsync(this.props.doujinshi.id, this.props.variant.id, this.props.index || 0);
   }
 }
