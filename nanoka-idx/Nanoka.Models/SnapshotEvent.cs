ce51aa0a1c68aa40db4e1a18249139693b48c595
phosphene47 <phosphene47@chiya.dev>
2019-09-12 04:06:40 +1000

    Refer to entity

--- nanoka-idx/Nanoka.Models/SnapshotEvent.cs
+++ nanoka-idx/Nanoka.Models/SnapshotEvent.cs
@@ -7,23 +7,23 @@ namespace Nanoka.Models
         [EnumMember(Value = "unspecified")] Unspecified = 0,
 
         /// <summary>
-        /// The snapshot was created when an object was created.
+        /// The snapshot was created when an entity was created.
         /// In this case, <see cref="Snapshot{T}.Value"/> is null.
         /// </summary>
         [EnumMember(Value = "create")] Creation = 1,
 
         /// <summary>
-        /// The snapshot was created when an object was modified.
+        /// The snapshot was created when an entity was modified.
         /// </summary>
         [EnumMember(Value = "modify")] Modification = 2,
 
         /// <summary>
-        /// The snapshot was created when an object was deleted.
+        /// The snapshot was created when an entity was deleted.
         /// </summary>
         [EnumMember(Value = "delete")] Deletion = 3,
 
         /// <summary>
-        /// The snapshot was created when an object was reverted to a previous snapshot.
+        /// The snapshot was created when an entity was reverted to a previous snapshot.
         /// </summary>
         [EnumMember(Value = "revert")] Rollback = 4
     }
