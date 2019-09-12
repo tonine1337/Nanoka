f35d2b5bd06043683458cdd1eec4c6f4167f8ff5
phosphene47 <phosphene47@chiya.dev>
2019-09-12 22:58:08 +1000

    Remove unspecified snapshot event

--- nanoka-idx/Nanoka.Models/SnapshotEvent.cs
+++ nanoka-idx/Nanoka.Models/SnapshotEvent.cs
@@ -4,27 +4,25 @@ namespace Nanoka.Models
 {
     public enum SnapshotEvent
     {
-        [EnumMember(Value = "unspecified")] Unspecified = 0,
-
         /// <summary>
         /// The snapshot was created when an entity was created.
         /// In this case, <see cref="Snapshot{T}.Value"/> is null.
         /// </summary>
-        [EnumMember(Value = "create")] Creation = 1,
+        [EnumMember(Value = "create")] Creation = 0,
 
         /// <summary>
         /// The snapshot was created when an entity was modified.
         /// </summary>
-        [EnumMember(Value = "modify")] Modification = 2,
+        [EnumMember(Value = "modify")] Modification = 1,
 
         /// <summary>
         /// The snapshot was created when an entity was deleted.
         /// </summary>
-        [EnumMember(Value = "delete")] Deletion = 3,
+        [EnumMember(Value = "delete")] Deletion = 2,
 
         /// <summary>
         /// The snapshot was created when an entity was reverted to a previous snapshot.
         /// </summary>
-        [EnumMember(Value = "revert")] Rollback = 4
+        [EnumMember(Value = "revert")] Rollback = 3
     }
 }
