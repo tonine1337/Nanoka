48118b474194b9a2f1921854d75a203203050b7b
phosphene47 <phosphene47@chiya.dev>
2019-09-12 11:38:57 +1000

    Remove Unknown entity type

--- nanoka-idx/Nanoka.Models/NanokaEntity.cs
+++ nanoka-idx/Nanoka.Models/NanokaEntity.cs
@@ -4,12 +4,11 @@ namespace Nanoka.Models
 {
     public enum NanokaEntity
     {
-        [EnumMember(Value = "unknown")] Unknown = 0,
-        [EnumMember(Value = "user")] User = 1,
-        [EnumMember(Value = "book")] Book = 2,
-        [EnumMember(Value = "image")] Image = 3,
-        [EnumMember(Value = "song")] Song = 4,
-        [EnumMember(Value = "snapshot")] Snapshot = 5,
-        [EnumMember(Value = "vote")] Vote = 6
+        [EnumMember(Value = "user")] User = 0,
+        [EnumMember(Value = "book")] Book = 1,
+        [EnumMember(Value = "image")] Image = 2,
+        [EnumMember(Value = "song")] Song = 3,
+        [EnumMember(Value = "snapshot")] Snapshot = 4,
+        [EnumMember(Value = "vote")] Vote = 5
     }
 }
