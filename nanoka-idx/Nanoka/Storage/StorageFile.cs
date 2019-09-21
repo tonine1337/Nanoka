049eb8b763089bd1056c985b407274ed6593150d
phosphene47 <phosphene47@chiya.dev>
2019-09-21 18:28:11 +1000

    Make StorageFile disposable

--- nanoka-idx/Nanoka/Storage/StorageFile.cs
+++ nanoka-idx/Nanoka/Storage/StorageFile.cs
@@ -1,11 +1,14 @@
+using System;
 using System.IO;
 
 namespace Nanoka.Storage
 {
-    public class StorageFile
+    public class StorageFile : IDisposable
     {
         public string Name { get; set; }
         public Stream Stream { get; set; }
         public string MediaType { get; set; }
+
+        public void Dispose() => Stream.Dispose();
     }
 }
